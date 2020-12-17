0d7e148d2f2106105890cb4ec12339c5438a657b
sergio posse <sergiodavidposse@gmail.com>
2020-12-16 23:11:22 -0300

    changin personal data to phpmailer

--- mail.php
+++ mail.php
@@ -5,6 +5,10 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+$msg=$_POST["mensaje"];
+$msg = wordwrap($msg,70);
+$asunto=$_POST["asunto"];
+$mail=$_POST["email"];
 // Load Composer's autoloader
 require 'vendor/autoload.php';
 
@@ -23,19 +27,19 @@ try {
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->setFrom($mail, 'Lo envio');
+    $mail->addAddress('sergiodavidposse@gmail.com', 'Sergio Posse');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = $asunto;
+    $mail->Body    = $msg;
+    $mail->AltBody = $msg;
 
     $mail->send();
     echo 'Message has been sent';
